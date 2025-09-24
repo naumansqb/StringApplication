bcc3fb3d72f2989ee746371a8d3a964b82d4fd8b
canexico <ns334@njit.edu>
2025-09-24 13:37:51 -0400

    Receipt.java: completion of step 3

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Receipt {
+    public static void main(String[] args) {
+        System.out.println();
+        String itemName;double price;int qty;
+        itemName= "oranges";
+        price=7.99;
+        qty=4;
+        System.out.println("You bought "+qty+" "+itemName+" for $"+price);
+
+    }
+}
